4052891827464baa7ea0c2bd54a87e300937d3eb
Ruslan Netesov <ruslann09@inbox.ru>
2018-02-15 15:29:13 +0700

    Initial commit

---
+++ Assets/Scripts/MovingActive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingActive : MonoBehaviour {
+	public Animator anim, anim1;
+	private WaitForSeconds timeToPlayBack1, timeToPlayBack2;
+
+	void Start () {
+		timeToPlayBack1 = new WaitForSeconds (1.0f);
+		timeToPlayBack2 = new WaitForSeconds (2.0f);
+
+		StartCoroutine (AirCondition1 ());
+		StartCoroutine (AirCondition2 ());
+	}
+
+	private IEnumerator AirCondition1 () {
+		yield return timeToPlayBack1;
+		anim.SetTrigger ("Start");
+	}
+
+	private IEnumerator AirCondition2 () {
+		yield return timeToPlayBack2;
+		anim1.SetTrigger ("Start");
+	}
+}
